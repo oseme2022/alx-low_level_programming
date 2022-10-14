85af58d9c76110fc68909baa1da20eb0fdbe2d8b
oseme2022 <jenniferakhagba@gmail.com>
2022-10-14 03:09:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,14 @@
 /**
  * main - Entry point
  *
- * return always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 { 
 
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 
 }
