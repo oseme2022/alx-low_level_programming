80e72b588c50bf969adf40ce78591791a2884765
oseme2022 <jenniferakhagba@gmail.com>
2022-10-17 14:18:44 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	char b;
+
+	a = '0';
+	b = 'a';
+
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	while
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+			putchar('\n');
+			return (0);
+		}
+
+
