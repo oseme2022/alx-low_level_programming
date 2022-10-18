87289d02444667295d93990285f69932fcde89c7
oseme2022 <jenniferakhagba@gmail.com>
2022-10-18 05:39:12 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowcase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter)
+_putchar('\n');
+}
+
 
