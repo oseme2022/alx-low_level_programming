0e99bd2b7fd292b1f59ea396c95600b3aa898ab3
oseme2022 <jenniferakhagba@gmail.com>
2022-10-24 14:46:15 -0700

    jjhhhbh

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>                                                                                                  
-#include <time.h> 
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,8 +8,9 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void rev_string(char *s);
-void puts_half(char *str)
+void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif 
