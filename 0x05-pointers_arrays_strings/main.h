0f8012ef6ddad8d7c0bb65a70bd1e0be81beb061
oseme2022 <jenniferakhagba@gmail.com>
2022-10-24 07:18:21 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +11,5 @@ void rev_string(char *s);
 void puts_half(char *str)
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif 
